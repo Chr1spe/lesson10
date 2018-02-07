9e16c98028dcaed3f766f0a740bc4e7a27f803dd
unknown <chrispe1217@gmail.com>
2018-02-06 19:52:04 -0500

    Tutorial Complere

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,13 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         view()->composer('layouts.sidebar', function ($view) {
-            $view->with('archives', \App\Post::archives());
-            $view->with('tags', \App\Tag::pluck('name'));
+            $archives = \App\Post::archives();
+            $tags = \App\Tag::has('posts')->pluck('name');
+
+            $view->with(compact('archives', 'tags'));
+
+            //$view->with('archives', \App\Post::archives());
+            //$view->with('tags', \App\Tag::has('posts')->pluck('name'));
         });
     }
 
